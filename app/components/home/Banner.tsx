370d07756b904bc532b70cb9dd007a65c8595031
ozzkan-burak <brk.ozzkan@gmail.com>
2024-03-07 23:15:57 +0300

    add fake data

--- app/components/home/Banner.tsx
+++ app/components/home/Banner.tsx
@@ -2,15 +2,9 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div className="h-[237px">
-      <div className="h-[137px]">
-        <Image
-          src={
-            "https://thumbs.dreamstime.com/b/single-blank-red-banner-ribbon-vector-isolated-multipurpose-text-template-109073118.jpg"
-          }
-          alt=""
-          fill
-        />
+    <div className="h-[237px bg-orange-300">
+      <div className="h-[137px] relative">
+        <Image src={"/banner.png"} alt="" fill />
       </div>
     </div>
   );
