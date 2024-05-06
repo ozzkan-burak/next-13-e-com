9a576a47d518660a7807857eb5c73dc8f5135c7c
ozzkan-burak <brk.ozzkan@gmail.com>
2024-05-06 23:43:16 +0300

    episode10

--- app/components/home/Banner.tsx
+++ app/components/home/Banner.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div className="h-[237px bg-orange-300">
+    <div className="h-[187px]">
       <div className="h-[137px] relative">
         <Image src={"/banner.png"} alt="" fill className="object-cover" />
       </div>
